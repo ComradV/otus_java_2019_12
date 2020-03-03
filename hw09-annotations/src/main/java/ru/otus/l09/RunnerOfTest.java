daa3640351bb51587c5c8e1ccca0957974366d45
Gleb Vasiliev <vasiliev.g.i@gmail.com>
2020-03-03 07:39:47 +0300

    Refactored. Now used constructor, help methods to avoid duplicating code

--- hw09-annotations/src/main/java/ru/otus/l09/RunnerOfTest.java
+++ hw09-annotations/src/main/java/ru/otus/l09/RunnerOfTest.java
@@ -7,71 +7,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RunnerOfTest {
+  private int success_counter;
+  private String className;
+  private List<Method> methodsBefore, methodsAfter, methodsTest;
+  private Constructor<?> constructor;
 
-  public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-    int success_counter = 0;
-
-    String className = args[0];
+  private RunnerOfTest(String className) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException{
+    this.className = className;
     System.out.println(className);
 //    String className = "ru.otus.l09.MyClassToTest";
     Class cl = Class.forName(className);
 
     Method[] method = cl.getMethods();
-    List<Method> methodsBefore = new ArrayList<>();
-    List<Method> methodsAfter = new ArrayList<>();
-    List<Method> methodsTest = new ArrayList<>();
-
-    for(Method md: method){
-      if(md.isAnnotationPresent(Test.class)) {
+    methodsBefore = new ArrayList<>();
+    methodsAfter = new ArrayList<>();
+    methodsTest = new ArrayList<>();
+    for (Method md : method) {
+      if (md.isAnnotationPresent(Test.class)) {
         methodsTest.add(md);
       }
-      if(md.isAnnotationPresent(Before.class)) {
+      if (md.isAnnotationPresent(Before.class)) {
         methodsBefore.add(md);
 
       }
-      if(md.isAnnotationPresent(After.class)) {
+      if (md.isAnnotationPresent(After.class)) {
         methodsAfter.add(md);
 
       }
     }
-    Constructor<?> constructor = cl.getConstructor(null);
+    constructor = cl.getConstructor(null);
 
-    for (Method methodTest: methodsTest){
+  }
+  private void makeTests() throws InstantiationException, InvocationTargetException, IllegalAccessException {
+    for (Method methodTest : methodsTest) {
       Object o = constructor.newInstance();
-      methodsBefore.stream().forEach(methodBefore -> {
-        try {
-          methodBefore.invoke(o);
-        } catch (IllegalAccessException e) {
-          e.printStackTrace();
-        } catch (InvocationTargetException e) {
-          e.printStackTrace();
-        }
-      });
-
-      try {
-        methodTest.invoke(o);
-        System.out.printf("Test "+methodTest.getName()+" success!");
-        success_counter++;
-      } catch (Exception e) {
-        e.printStackTrace();
-        System.out.printf("Test "+methodTest.getName()+" failed!");
-      }
-
-      methodsAfter.stream().forEach(methodAfter -> {
-        try {
-          methodAfter.invoke(o);
-        } catch (IllegalAccessException e) {
-          e.printStackTrace();
-        } catch (InvocationTargetException e) {
-          e.printStackTrace();
-        }
-      });
-
+      runMethods(o, methodsBefore);
+      makeSingleTest(o, methodTest);
+      runMethods(o, methodsAfter);
     }
-    System.out.println("Class " + className+" test results:");
-    System.out.println("Tests performed: " + methodsTest.size());
-    System.out.printf("Success: " + success_counter);
-    System.out.printf("Failed: " + (methodsTest.size() - success_counter));
 
+  }
+
+  private void makeSingleTest(Object o, Method methodTest) {
+    try {
+      methodTest.invoke(o);
+      System.out.printf("Test " + methodTest.getName() + " success!");
+      success_counter++;
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.printf("Test " + methodTest.getName() + " failed!");
+    }
+  }
+
+  public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+
+    String className = args[0];
+    RunnerOfTest tester = new RunnerOfTest(className);
+    tester.makeTests();
+    tester.printTestStatistics();
+
+  }
+
+  private void printTestStatistics() {
+    System.out.println("Class " + className + " test results:");
+    System.out.println("Tests performed: " + methodsTest.size());
+    System.out.println("Success: " + success_counter);
+    System.out.println("Failed: " + (methodsTest.size() - success_counter));
+  }
+
+  private static void runMethods (Object o, List<Method> methods) throws IllegalAccessException, InvocationTargetException{
+    methods.stream().forEach(methodBefore -> {
+      try {
+        methodBefore.invoke(o);
+      } catch (IllegalAccessException | InvocationTargetException e) {
+        e.printStackTrace();
+//        throw e;
+      }
+    });
   }
 }
